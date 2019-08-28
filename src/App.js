98dea171c587b60a51fa98664e766aac648adf9e
Kyle Leonard <kdleonard@icloud.com>
2019-08-27 19:24:10 -0500

    Added state and useEffect along with fixing emoji

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import axios from "axios";
 
 function App(props) {
+  const [nasaPhoto, setNasaPhoto] = useState([]);
 
-  
+  useEffect(() => {
+    axios
+      .get("https://api.nasa.gov/planetary/apod")
+      .then(response => {
+        setNasaPhoto(response.data);
+        console.log(`Response: ${response.data}`);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, []);
+
   return (
     <div className="App">
       <p>
         Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
+        app! Have fun{" "}
+        <span role="img" aria-label="jsx-a11y/accessible-emoji">
+          🚀
+        </span>
+        !
       </p>
     </div>
   );
